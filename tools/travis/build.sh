c8e8f31eb3ade287e6f787312de4dae5e01341cb
Chetan Mehrotra <chetanm@apache.org>
2018-02-20 12:01:27 +0530

    Use Maven based dependency instead of gradle project based dependency (#17)

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -11,14 +11,6 @@ export OPENWHISK_HOME=$WHISKDIR
 
 IMAGE_PREFIX="testing"
 
-# Build runtime
-cd $ROOTDIR
-TERM=dumb ./gradlew \
-:core:actionProxy:distDocker \
-:sdk:docker:distDocker \
--PdockerImagePrefix=${IMAGE_PREFIX}
-
-
 # Build OpenWhisk
 cd $WHISKDIR
 
@@ -29,6 +21,19 @@ docker pull openwhisk/invoker
 docker tag openwhisk/invoker ${IMAGE_PREFIX}/invoker
 docker pull openwhisk/nodejs6action
 docker tag openwhisk/nodejs6action ${IMAGE_PREFIX}/nodejs6action
+
+TERM=dumb ./gradlew \
+:common:scala:install \
+:core:controller:install \
+:core:invoker:install \
+:tests:install
+
+# Build runtime
+cd $ROOTDIR
+TERM=dumb ./gradlew \
+:core:actionProxy:distDocker \
+:sdk:docker:distDocker \
+-PdockerImagePrefix=${IMAGE_PREFIX}
 
 
 

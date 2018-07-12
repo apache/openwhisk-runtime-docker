6e43689018e86ae53dcfbac8c92faad906a74b93
rodric rabbah <rodric@gmail.com>
2018-07-12 16:55:34 -0400

    Remove unnecessary Travis setup. (#53)
    * Travis cleanup
    * Tag master build as 'latest'.

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -31,4 +31,5 @@ git clone https://github.com/apache/incubator-openwhisk-utilities.git
 # clone main openwhisk repo. for testing purposes
 git clone --depth=1 https://github.com/apache/incubator-openwhisk.git openwhisk
 cd openwhisk
+
 ./tools/travis/setup.sh
